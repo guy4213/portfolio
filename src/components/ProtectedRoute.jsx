0a587781329ee93ec94ab360211e67e6f4f35b66
guy4213 <guy421367@gmail>
2025-10-07 21:09:52 +0300

    .

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export const ProtectedRoute = ({ children }) => {
